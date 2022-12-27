a1e12ab0504d0f61d4b4fc17de6a7cd153b35756
kshshton <michkrzyszton@gmail.com>
2022-12-27 16:33:53 +0100

    Added styling, fixed dependencies

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask, request, render_template
 from scrape.wiki import date_of_birth
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
 def index():
-    return  render_template('index.html')
+    return render_template('index.html', form='form.html')
 
 
 @app.route('/', methods=['POST'])
 def post():
     first_name = request.form['first_name']
     last_name = request.form['last_name']
-    return date_of_birth(first_name, last_name)
+    return render_template('index.html', date=date_of_birth(first_name, last_name))
 
 
 def main():
