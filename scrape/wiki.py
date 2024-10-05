3cb837ad45add0cd1041bc7a6a1751a981bfc782
kshshton <96781803+kshshton@users.noreply.github.com>
2024-10-05 13:14:53 +0200

    Improved get_date_of_birth function

--- scrape/wiki.py
+++ scrape/wiki.py
@@ -30,18 +30,13 @@ class Person:
 
     def get_date_of_birth(self) -> str:
         '''Scraping person's date of birth'''
-        tr_tags = self.soup.select('tr')
-        title_attrib_pattern = re.compile(r'.*?title="(\d[^"]+)')
+        tr_tag_str = next(str(tr) for tr in soup.find_all('tr') if 'miejsce urodzenia' in str(tr))
+        title_attrib_pattern = re.compile(r'title="(\d[^"]+)')
+        alternative_pattern = re.compile(r'>(\d[^<]+)')
+        result = " ".join(title_attrib_pattern.finditer(tr_tag_str))
+        return result if result else alternative_pattern.search(tr_tag_str).group(1)
 
-        for tag in tr_tags:
-            if 'miejsce urodzenia' in str(tag):
-                result = ''
-                for match in title_attrib_pattern.finditer(str(tag)):
-                    result += match.group(1) + ' '
-                if result == '':
-                    result = re.search('.*?>(\d[^<]+)', str(tag)).group(1)
-                return result.strip()
-
+    
     def get_image(self) -> str:
         '''Scraping person's image'''
         tr_tag_with_image = self.soup.find('tr', {'class': 'grafika iboxs'})
